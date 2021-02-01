56b7f6cb1f4ee4546e579f9aa5ade292f1256b59
Chris Umbach <chrisumbach@gmail.com>
2021-01-31 22:32:11 -0800

    middleware and auth

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//
+//   } else {
+//     next();
+//   }
+// })
+
+// app.use((req, res, next) => {
+//   res.status(503).send('Server under maintenance');
+// })
+
+
+
 
 app.use(express.json());
 app.use(userRouter);
@@ -15,3 +28,15 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 })
+
+// const jwt = require('jsonwebtoken');
+//
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '0 second'});
+//   console.log(token);
+//
+//   const data = jwt.verify(token, 'thisismynewcourse');
+//   console.log(data);
+// }
+//
+// myFunction();
