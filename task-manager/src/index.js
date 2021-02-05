407b9db170b48a9eaa5f535f59059db6f53fea9b
Chris Umbach <chrisumbach@gmail.com>
2021-02-04 23:15:26 -0800

    sort, paginate, filter

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,8 +19,6 @@ const port = process.env.PORT || 3000;
 // })
 
 
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -29,14 +27,17 @@ app.listen(port, () => {
   console.log('Server is up on port ' + port);
 })
 
-// const jwt = require('jsonwebtoken');
-//
-// const myFunction = async () => {
-//   const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '0 second'});
-//   console.log(token);
-//
-//   const data = jwt.verify(token, 'thisismynewcourse');
-//   console.log(data);
-// }
-//
-// myFunction();
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const main = async () => {
+  // const task = await Task.findById('6018d74e310b416255567ba4');
+  // await task.populate('owner').execPopulate();
+  // console.log(task.owner);
+
+  const user = await User.findById('6018d4ff396bd160efc4c0fd');
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+}
+
+// main();
