3a2d59ad6ba6ecfdc6743b7567913473b6b0139e
Chris Umbach <chrisumbach@gmail.com>
2021-02-05 18:17:19 -0800

    file uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,38 +6,10 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//
-//   } else {
-//     next();
-//   }
-// })
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Server under maintenance');
-// })
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
-})
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-  // const task = await Task.findById('6018d74e310b416255567ba4');
-  // await task.populate('owner').execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById('6018d4ff396bd160efc4c0fd');
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks);
-}
-
-// main();
+});
